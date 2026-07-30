a40f07bfe5a1db4370137e8fa6ae4cdeca95310e
shengzhangsz <15647997+shengzhangsz@user.noreply.gitee.com>
2026-07-30 13:59:18 +0800

    fix: 修正今日考卷 URL 为相对路径

--- pwa/sw.js
+++ pwa/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'exammaster-v13';
+const CACHE = 'exammaster-v14';
 const ASSETS = ['./', './index.html', './manifest.json', './questions.json'];
 
 self.addEventListener('install', e => {
