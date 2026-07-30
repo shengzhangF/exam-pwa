988ffa87c8d8f81408ee6333b49716a0064c4136
shengzhangsz <15647997+shengzhangsz@user.noreply.gitee.com>
2026-07-30 14:13:15 +0800

    fix: 考试流程全部使用自定义弹窗，修复考卷 URL

--- pwa/sw.js
+++ pwa/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'exammaster-v14';
+const CACHE = 'exammaster-v15';
 const ASSETS = ['./', './index.html', './manifest.json', './questions.json'];
 
 self.addEventListener('install', e => {
