75aa0b4168e143bced40c5a98c0e05de35d6ac00
shengzhangsz <15647997+shengzhangsz@user.noreply.gitee.com>
2026-07-30 14:44:52 +0800

    fix: SW注册加时间戳强制更新

--- pwa/sw.js
+++ pwa/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'exammaster-v15';
+const CACHE = 'exammaster-v16';
 const ASSETS = ['./', './index.html', './manifest.json', './questions.json'];
 
 self.addEventListener('install', e => {
